bae807b05b7d04f4769ee428b6ca0d6fda3f4280
Paat Sinsuwan <paat@sinsuwan.com>
2017-01-10 17:03:32 -0500

    Add cache data

--- routes/feeds/index.js
+++ routes/feeds/index.js
@@ -2,34 +2,18 @@ var express = require('express');
 var request = require('request');
 var parser = require('xml2json');
 var _ = require('lodash');
+var fs = require('fs');
+var path = require('path');
 var Q = require('q');
 var bluemixUrl = "https://www.ibm.com/blogs/bluemix/category/whats-new/feed/";
 var bluemixAPIUrl = "https://www.ibm.com/blogs/bluemix/wp-json/posts";
+var CAAPIUrl = "https://www.ibm.com/blogs/cloud-announcements/wp-json/posts";
 var middlewareUrl = "https://www.ibm.com/blogs/cloud-announcements/feed/";
 var feedDataItems = [];
+var bluemixAPIData = [];
+var CAAPIData = [];
+var page = 1;
 var router = express.Router();
-
-
-_.mixin({
-  'orderKeysBy': function (obj, comparator, asc) {
-    var keys = _.orderBy(_.keys(obj), function (key) {
-      return comparator ? comparator(obj[key], key) : key;
-    }, asc);
-
-    return _.zipObject(keys, _.map(keys, function (key) {
-      return obj[key];
-    }));
-  },
-  'reverseCollection': function (obj){
-    var keys = Object.keys(obj);
-    var tempCol = {};
-
-    for(var i = keys.length-1; i >= 0; i--){
-      tempCol[keys[i]] = obj[keys[i]];
-    }
-    return tempCol;
-  }
-});
 
 var getPromises = function(url){
   var deferred = Q.defer();
@@ -45,6 +29,97 @@ var getPromises = function(url){
 
   return deferred.promise;
 };
+
+var promiseWhilePromise = function(condition, action){
+  var deferred = Q.defer();
+  function loop(){
+    condition().then(function(bool){
+      if(!bool){
+        return deferred.resolve();
+      }
+      else {
+        return Q.when(action(), loop, deferred.reject);
+      }
+    });
+  }
+  Q.nextTick(loop);
+  return deferred.promise;
+}
+
+var getBluemixData = function(){
+  var deferred = Q.defer();
+  var currentUrl = bluemixAPIUrl+"?page="+page;
+  request.get(currentUrl, function(err, response, body){
+    if(!err && response.statusCode == 200){
+      var tempData = JSON.parse(body);
+      var filtered = tempData.filter(function(o){
+        var cats = o.terms.category.filter(function(oo){
+          // what's new category ID on Bluemix blog
+          return oo.ID == 1963;
+        });
+        if(cats.length) return o;
+      });
+      bluemixAPIData = bluemixAPIData.concat(filtered);
+      deferred.resolve(page <= response.headers["x-wp-totalpages"]);
+      // dev testing number of page
+      // deferred.resolve(page <= 1);
+    }
+    else {
+      deferred.reject(new Error(err));
+    }
+  });
+  return deferred.promise;
+}
+
+var getCAData = function(){
+  var deferred = Q.defer();
+  var currentUrl = CAAPIUrl+"?page="+page;
+  request.get(currentUrl, function(err, response, body){
+    if(!err && response.statusCode == 200){
+      // var tempData = JSON.parse(body);
+      // var filtered = tempData.filter(function(o){
+      //   var cats = o.terms.category.filter(function(oo){
+      //     // what's new category ID on Bluemix blog
+      //     return oo.ID == 1963;
+      //   });
+      //   if(cats.length) return o;
+      // });
+      // CAAPIData = bluemixAPIData.concat(filtered);
+      CAAPIData = CAAPIData.concat(JSON.parse(body));
+      deferred.resolve(page <= response.headers["x-wp-totalpages"]);
+      // dev testing number of page
+      // deferred.resolve(page <= 1);
+    }
+    else {
+      deferred.reject(new Error(err));
+    }
+  });
+  return deferred.promise;
+}
+
+var morePage = function(){
+  console.log(page);
+  page++;
+}
+
+var setFirstPage = function(){
+  page = 1;
+}
+
+var writeToFilePromise = function(writePath, data){
+  var deferred = Q.defer();
+
+  fs.writeFile(writePath, data, function(err){
+    if(err){
+      deferred.reject(new Error(err));
+    }
+    else {
+      deferred.resolve();
+    }
+  })
+
+  return deferred.promise;
+}
 
 router.use('/by-month', function(req, res, next){
   feedDataItems = [];
@@ -63,15 +138,6 @@ router.use('/by-month', function(req, res, next){
     var groupedByYear = _.groupBy(feedDataItems, function(o){
       return new Date(o.pubDate).getFullYear();
     });
-
-
-
-    
-    // _.forEach(grouped, function(o){
-    //   o = _.orderBy(o, function(oo){
-    //     return new Date(oo);
-    //   });
-    // });
     res.header('Content-Type', 'application/json')
     .send(JSON.stringify(grouped));
   });
@@ -104,6 +170,34 @@ router.use('/new', function (req, res, next) {
     res.header('Content-Type', 'application/json')
     .send(JSON.stringify(sorted));
   });
+});
+
+router.use('/getBluemix', function(req, res, next){
+  promiseWhilePromise(getBluemixData, morePage).then(function(){
+    console.log("done retrieving!!!!")
+    setFirstPage();
+    return writeToFilePromise(path.join(process.cwd(), "public", "files", "bluemix.json"), JSON.stringify(bluemixAPIData));
+  })
+  .then(function(){
+    console.log("file written");
+  })
+  .done(function(){
+    res.send("done");
+  })
+})
+
+router.use('/getCA', function(req, res, next){
+  promiseWhilePromise(getCAData, morePage).then(function(){
+    console.log("done retrieving!!!!")
+    setFirstPage();
+    return writeToFilePromise(path.join(process.cwd(), "public", "files", "ca.json"), JSON.stringify(CAAPIData));
+  })
+  .then(function(){
+    console.log("file written");
+  })
+  .done(function(){
+    res.send("done");
+  })
 })
 
 router.use('/', function(req, res, next){
@@ -126,5 +220,7 @@ router.use('/', function(req, res, next){
     .send(JSON.stringify(sorted));
   });
 });
+
+
 
 module.exports = router;
